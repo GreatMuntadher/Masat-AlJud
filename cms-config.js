a5f8937013ecc023c1fafe824244d007035ad0a3
Muntadher Al-Hafidh <greatmuntadher@gmail.com>
2026-08-04 16:28:12 +0300

    Delete cms-config.js

--- cms-config.js
+++
@@ -1,2 +0,0 @@
-/* Public endpoint only. Secrets are Script Properties and never belong here. */
-window.MHR_CMS_API_URL = "https://script.google.com/macros/s/AKfycbyCksO81iKnzikVq8M9S-jOrEPrlWgNcjDTs0bgeGDWWmiL8OMLWLKuOTKSl0oKkhpHrQ/exec";
